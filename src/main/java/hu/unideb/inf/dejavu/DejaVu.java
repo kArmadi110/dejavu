a65cf0e8731b4cd19ae9312700baacd2abbdf251
iam346 <badogergo@gmail.com>
2016-02-26 21:02:23 +0100

    ops

--- src/main/java/hu/unideb/inf/dejavu/DejaVu.java
+++ src/main/java/hu/unideb/inf/dejavu/DejaVu.java
@@ -16,6 +16,7 @@ import hu.unideb.inf.dejavu.gui.Congrat;
 import hu.unideb.inf.dejavu.gui.DVMenu;
 import hu.unideb.inf.dejavu.gui.ExitToolbar;
 import hu.unideb.inf.dejavu.gui.SlidePane;
+import hu.unideb.inf.dejavu.gui.WelcomeMenu;
 
 
 //TODO: stopwatch
@@ -90,7 +91,7 @@ public class DejaVu extends Application {
 		stage = arg;
 		stage.initStyle(StageStyle.TRANSPARENT);
 
-		setNewMenu(new Congrat());
+		setNewMenu(new WelcomeMenu());
 		stage.setTitle("dejaVu");
 
 		stage.setMaxHeight(600);// nem túl szép
